f3fc1e4bba88a6a7cb8c8945a57739d17e9e2b52
pandakraut <75352338+pandakraut@users.noreply.github.com>
2021-12-03 12:28:49 -0500

    Add files via upload

---
+++ day3_1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace AdventOfCode2020
+{
+    class day3_1
+    {
+        public static List<int> depths = new List<int>();
+
+        static void Main(string[] args)
+        {
+            string gamma = "";
+            string episolon = "";
+            int totalLines = 0;
+            List<int> counts = new List<int>
+            {
+                0,0,0,0,0,0,0,0,0,0,0,0
+                //0,0,0,0,0
+            };
+            int count = 0;
+            foreach (string line in File.ReadLines("../../../InputDay3.txt"))
+            {
+                totalLines++;
+                count = 0;
+                foreach (char a in line)
+                {
+                    counts[count] += Convert.ToInt32(a.ToString());
+                    count++;
+                }
+            }
+
+            foreach (int total in counts)
+            {
+                if (totalLines - total < total)
+                {
+                    gamma += "1";
+                    episolon += "0";
+                }
+                else
+                {
+                    gamma += "0";
+                    episolon += "1";
+                }
+            }
+
+            Console.WriteLine("output: " + Convert.ToInt32(gamma, 2) * Convert.ToInt32(episolon, 2));
+        }
+    }
+}
---
+++ day3_2.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace AdventOfCode2020
+{
+    class day3_2
+    {
+        public static List<char[]> values = new List<char[]>();
+
+        static void Main(string[] args)
+        {
+            int oxIndex = 0;
+            int co2Index = 0;
+
+            List<int> indexes = new List<int>();
+            int count = 0;
+            foreach (string line in File.ReadLines("../../../InputDay3.txt"))
+            {
+                values.Add(line.ToCharArray());
+                indexes.Add(count);
+                count++;
+            }
+
+            oxIndex = findValue(indexes, 0, true);
+            co2Index = findValue(indexes, 0, false);
+
+            Console.WriteLine("output: " + Convert.ToInt32(new string(values[oxIndex]), 2) * Convert.ToInt32(new string(values[co2Index]), 2));
+
+        }
+        public static int findValue(List<int> currentList, int index, bool mostCommon)
+        {
+            List<int> newOnes = new List<int>();
+            List<int> newZeroes = new List<int>();
+            int onesCount = 0;
+            int zeroCount = 0;
+            if (currentList.Count > 1)
+            {
+                foreach (int i in currentList)
+                {
+                    if (values[i][index] == '1')
+                    {
+                        newOnes.Add(i);
+                        onesCount++;
+                    }
+                    else
+                    {
+                        newZeroes.Add(i);
+                        zeroCount++;
+                    }
+                }
+                if (mostCommon)
+                {
+                    if (onesCount < zeroCount)
+                    {
+                        return findValue(newZeroes, ++index, mostCommon);
+                    }
+                    return findValue(newOnes, ++index, mostCommon);
+                }
+                else
+                {
+                    if (onesCount < zeroCount)
+                    {
+                        return findValue(newOnes, ++index, mostCommon);
+                    }
+                    return findValue(newZeroes, ++index, mostCommon);
+                }
+            }
+            else
+            {
+                return currentList[0];
+            }
+        }                         
+    }
+}
