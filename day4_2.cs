08595df32dfdd8a4c97fc16fa8393c92bb86c0ce
pandakraut <75352338+pandakraut@users.noreply.github.com>
2021-12-04 23:47:19 -0500

    Add files via upload

---
+++ day4_2.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace AdventOfCode2020
+{
+    class day4_2
+    {
+        public static List<int> depths = new List<int>();
+
+        static void Main(string[] args)
+        {
+            List<string> drawList = new List<string>();
+            List<KeyValuePair<int, bool>> lineComponents = new List<KeyValuePair<int, bool>>();
+            List<List<KeyValuePair<int, bool>>> tempBingo = new List<List<KeyValuePair<int, bool>>>();
+            List<bingoBoard> bingoBoardList = new List<bingoBoard>();
+            int boardCount = 0;
+
+            foreach (string line in File.ReadLines("../../../InputDay4.txt"))
+            {
+                if (drawList.Count == 0)
+                {
+                    drawList = new List<string>(line.Split(','));
+                    continue;
+                }
+
+                if (line.Length == 0)
+                {
+                    if (tempBingo.Count > 0)
+                    {
+                        bingoBoardList.Add(new bingoBoard(boardCount, tempBingo));
+                        tempBingo = new List<List<KeyValuePair<int, bool>>>();
+                        boardCount++;
+                    }
+                }
+                else
+                {
+                    lineComponents = new List<KeyValuePair<int, bool>>();
+                    foreach (string currentComponent in line.Split(' '))
+                    {
+                        if (currentComponent.Length > 0)
+                        {
+                            lineComponents.Add(new KeyValuePair<int, bool>(Convert.ToInt32(currentComponent), false));
+                        }
+                    }
+                    tempBingo.Add(lineComponents);
+                }
+            }
+            bingoBoardList.Add(new bingoBoard(boardCount, tempBingo));
+
+            int bingoId = 0;
+            int output = 0;
+            int lastMatch = 0;
+            foreach (string currentDraw in drawList)
+            {
+                int match = Convert.ToInt32(currentDraw);
+                bool bingo = false;
+
+                foreach (bingoBoard currentBoard in bingoBoardList)
+                {
+                    if (!currentBoard.matched)
+                    { 
+                        bool matchFound = false;
+                        for (int i = 0; i < currentBoard.boardValues.Count; i++)
+                        {
+                            for (int j = 0; j < currentBoard.boardValues[i].Count; j++)
+                            {
+                                if (match == currentBoard.boardValues[i][j].Key)
+                                {
+                                    matchFound = true;
+                                    currentBoard.boardValues[i][j] = new KeyValuePair<int, bool>(currentBoard.boardValues[i][j].Key, true);
+                                    currentBoard.rowMatches[i]++;
+                                    if (currentBoard.rowMatches[i] == 5)
+                                    {
+                                        bingo = true;
+                                    }
+                                    currentBoard.columnMatches[j]++;
+                                    if (currentBoard.columnMatches[j] == 5)
+                                    {
+                                        bingo = true;
+                                    }
+                                    break;
+                                }
+                            }
+                            if (matchFound)
+                            {
+                                break;
+                            }
+                        }
+                        if (bingo)
+                        {
+                            output = 0;
+                            lastMatch = match;
+                            bingoId = currentBoard.boardId;
+                            currentBoard.matched = true;                        
+                            for (int i = 0; i < currentBoard.boardValues.Count; i++)
+                            {
+                                for (int j = 0; j < currentBoard.boardValues[i].Count; j++)
+                                {
+                                    if (!currentBoard.boardValues[i][j].Value)
+                                    {
+                                        output += currentBoard.boardValues[i][j].Key;
+                                    }
+                                }
+                            }
+                            bingo = false;
+                        }
+                    }
+                }                
+            }
+            Console.WriteLine("output: " + output * lastMatch);
+
+
+        }
+
+        public class bingoBoard
+        {
+            public int boardId { get; set; }
+            public List<List<KeyValuePair<int, bool>>> boardValues { get; set; }
+            public List<int> rowMatches { get; set; }
+            public List<int> columnMatches { get; set; }
+
+            public bool matched { get; set; }
+
+            public bingoBoard(int inputBoardId, List<List<KeyValuePair<int, bool>>> inputBoardValues)
+            {
+                boardId = inputBoardId;
+                boardValues = inputBoardValues;
+                rowMatches = new List<int>
+                {
+                    0,0,0,0,0
+                };
+                columnMatches = new List<int>
+                {
+                    0,0,0,0,0
+                };
+                matched = false;
+            }
+        }
+    }
+}
