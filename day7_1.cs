c80088c8ea42949ab4e6f3ab983a7dc435b15355
pandakraut <75352338+pandakraut@users.noreply.github.com>
2021-12-07 01:42:00 -0500

    Add files via upload

---
+++ day7_1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace AdventOfCode2020
+{
+    class day7_1
+    {
+
+        public static Dictionary<string, int> clouds = new Dictionary<string, int>();
+        public static HashSet<string> maxOverlapIndexes = new HashSet<string>();
+        public static int maxOverlaps = 2;
+        static void Main(string[] args)
+        {
+            List<int> crabs = new List<int>();
+            int median = 0;
+            int sum = 0;
+            foreach (string line in File.ReadLines("../../../InputDay7.txt"))
+            {
+                foreach (string crab in line.Split(','))
+                {
+                    crabs.Add(Convert.ToInt32(crab));
+                }
+            }
+            crabs.Sort();
+            if ((crabs.Count - 1) % 2 == 0)
+            {
+                // count is even, average two middle elements
+                int a = crabs[(crabs.Count - 1) / 2 - 1];
+                int b = crabs[(crabs.Count - 1) / 2];
+                median = (a + b) / 2;
+            }
+            else
+            {
+                // count is odd, return the middle element
+                median = crabs[crabs.Count / 2];
+            }
+
+            foreach (int crab in crabs)
+            {
+                sum += Math.Abs(crab - median);
+            }
+            Console.WriteLine("output: " + sum);
+        }
+    }
+}
---
+++ day7_2 .cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace AdventOfCode2020
+{
+    class day7_1
+    {
+
+        public static Dictionary<string, int> clouds = new Dictionary<string, int>();
+        public static HashSet<string> maxOverlapIndexes = new HashSet<string>();
+        public static int maxOverlaps = 2;
+        static void Main(string[] args)
+        {
+            List<int> crabs = new List<int>();
+            int mean = 0;
+            int total = 0;
+            int sum = 0;
+            foreach (string line in File.ReadLines("../../../InputDay7.txt"))
+            {
+                foreach (string crab in line.Split(','))
+                {
+                    crabs.Add(Convert.ToInt32(crab));
+                    total += Convert.ToInt32(crab);
+                }
+            }
+
+            mean = (int)Math.Ceiling((double)(total / (crabs.Count)));            
+
+            foreach (int crab in crabs)
+            {
+                int pos = Math.Abs(crab - mean);
+                sum += (pos * (pos + 1)) / 2;
+            }
+            Console.WriteLine("output: " + sum);
+        }
+    }
+}
