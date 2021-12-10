8f3bbde24ce1d8dd3161000b9075c699ff20283f
pandakraut <75352338+pandakraut@users.noreply.github.com>
2021-12-10 01:31:28 -0500

    Add files via upload

---
+++ day10_1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace AdventOfCode2020
+{
+    class day10_1
+    {
+
+        static void Main(string[] args)
+        {
+            int count = 0;
+            Stack<string> currentChars = new Stack<string>();
+
+            Dictionary<string, string> mapping = new Dictionary<string, string>()
+            {
+                {")", "("},
+                {"]", "["},
+                {"}", "{"},
+                {">", "<"}
+            };
+
+            Dictionary<string, int> scoreMapping = new Dictionary<string, int>()
+            {
+                {")", 3},
+                {"]", 57},
+                {"}", 1197},
+                {">", 25137}
+            };
+
+            string curChar;
+            int score = 0;
+            foreach (string line in File.ReadLines("../../../InputDay10.txt"))
+            {
+                foreach (char c in line)
+                {
+                    curChar = c.ToString();
+                    if (mapping.ContainsKey(curChar))
+                    {
+                        //closing char
+                        if (mapping[curChar] == currentChars.Peek())
+                        {
+                            //valid
+                            currentChars.Pop();
+                        }
+                        else
+                        {
+                            //corrupted
+                            score += scoreMapping[curChar];
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        //open char
+                        currentChars.Push(curChar);
+                    }
+                }
+            }
+
+            Console.WriteLine("output: " + score);
+        }
+    }
+}
---
+++ day10_2.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace AdventOfCode2020
+{
+    class day10_1
+    {
+
+        static void Main(string[] args)
+        {
+            Stack<string> currentChars = new Stack<string>();
+            List<long> completedLines = new List<long>();
+
+            Dictionary<string, string> mapping = new Dictionary<string, string>()
+            {
+                {")", "("},
+                {"]", "["},
+                {"}", "{"},
+                {">", "<"}
+            };
+
+            Dictionary<string, string> reverseMapping = new Dictionary<string, string>()
+            {
+                {"(", ")"},
+                {"[", "]"},
+                {"{", "}"},
+                {"<", ">"}
+            };
+
+            Dictionary<string, int> scoreMapping = new Dictionary<string, int>()
+            {
+                {")", 1},
+                {"]", 2},
+                {"}", 3},
+                {">", 4}
+            };
+
+            string curChar;
+            long score = 0;
+            bool corrupted;
+            foreach (string line in File.ReadLines("../../../InputDay10.txt"))
+            {
+                score = 0;
+                corrupted = false;
+                currentChars.Clear();
+                foreach (char c in line)
+                {
+                    curChar = c.ToString();
+                    if (mapping.ContainsKey(curChar))
+                    {
+                        //closing char
+                        if (mapping[curChar] == currentChars.Peek())
+                        {
+                            //valid
+                            currentChars.Pop();
+                        }
+                        else
+                        {
+                            //corrupted
+                            corrupted = true;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        //open char
+                        currentChars.Push(curChar);
+                    }
+                }
+
+                if (!corrupted)
+                {
+                    while (currentChars.Count > 0)
+                    {
+                        string updatedLine = line + reverseMapping[currentChars.Peek()];
+                        score *= 5;
+                        score += scoreMapping[reverseMapping[currentChars.Peek()]];
+                        currentChars.Pop();
+                    }
+                    completedLines.Add(score);
+                }
+            }
+            completedLines.Sort();            
+            long result = completedLines[(int)Math.Ceiling((double)(completedLines.Count / 2))];
+
+            Console.WriteLine("output: " + result);
+        }
+    }
+}
